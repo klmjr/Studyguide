1d7cf2daba763ff4424fdbb05f8cad4d0be2ba1f
klmjr <36527096+klmjr@users.noreply.github.com>
2018-03-15 15:14:30 -0400

    Study up!

---
+++ MidtermStudyGuide/src/studyGuideone.java
@@ -0,0 +1,42 @@
+
+public class studyGuideone {
+	
+	/*Key terms gone over during the practice quiz in calss.
+	 * 
+	 *  
+	 *  Abstract method: can only be declared in an abstract class. 
+	 * Must be fully implemented in any class that extends the abstract class.
+	 * can include access modifiers. Not an interface
+	 * 
+	 * Private, protected and public access modifiers are shared through different
+	 * classes in the same package
+	 * 
+	 * Overloading: Changes the implementation of a non-constructor method in the superclass
+	 * Annotation is optional, but best practice
+	 * Cannot restrict the visibility of the method
+	 * 
+	 * Encapsulation:Instance attributes are private. 
+	 * Using getter and setter methods to manipulate the contents of the
+	 * instance variable
+	 * 
+	 * Interface:Not a class but compiles with a .class extension
+	 * Provides a rule set a list of methods that must be implemented
+	 * Method sigantures are used, but no access modifiers
+	 * 
+	 * Final:Declare variable so it cant be extended or overridden, value must be
+	 * set in declaration
+	 * Declare a Class so it cant be extended
+	 * Declare a method so it cant be overridden (but can be called by subclass)
+	 * 
+	 * How do you call a no arg constructor through a 2 arg constructor? using this()
+	 * 
+	 * What does maven do? Handles external dependencies. Handles compile. installation
+	 * Executes unit testing
+	 * 
+	 * enum:Special data type that enables for a variable to be set of predefined constants
+	 * The variable must be equal to one of the values that have been predefined for it.
+	 * Like ranks and suits
+	 * 
+	 */
+
+}
